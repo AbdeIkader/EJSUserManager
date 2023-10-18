6b577b0a80e64b40c837afcc9702b58e3262c960
Abdelkader <abdelrahmanabdelkader2002@gmail.com>
2023-10-19 01:27:50 +0300

    Add user feature

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
-import express from 'express'
+import path from 'path'
 import { dbConnection } from './DB/dbConnection.js';
+import { userModel } from './DB/models/user.model.js';
 
 const app = express()
 
 const PORT = 3000;
 
 app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
 
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(path.resolve(),'index.html'))
+})
+
+app.post('/addUser',async(req,res)=>{
+    console.log(req.body);
+    let result = new userModel(req.body)
+    await result.save()
+    res.redirect('/')
+})
 
 dbConnection()
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT || PORT}`);
 })
