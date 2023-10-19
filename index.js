8fb8b0ea3208afdfaeef6b2a307c8e836b1b719a
Abdelkader <abdelrahmanabdelkader2002@gmail.com>
2023-10-19 03:19:17 +0300

    Add user functionality with EJS

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-dotenv.config()
-import path from 'path'
-import { dbConnection } from './DB/dbConnection.js';
-import { userModel } from './DB/models/user.model.js';
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import path from "path";
+import { dbConnection } from "./DB/dbConnection.js";
+import { userModel } from "./DB/models/user.model.js";
 
-const app = express()
+const app = express();
 
 const PORT = 3000;
 
-app.use(express.json())
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(path.resolve(),'index.html'))
-})
+app.get("/users", async (req, res) => {
+  let result = await userModel.find();
 
-app.post('/addUser',async(req,res)=>{
-    console.log(req.body);
-    let result = new userModel(req.body)
-    await result.save()
-    res.redirect('/')
-})
+  res.render("users.ejs", { result });
+});
 
-dbConnection()
-app.listen(process.env.PORT || PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT || PORT}`);
-})
+app.get("/about", async (req, res) => {
+  res.render("about.ejs");
+});
+
+app.post("/addUser", async (req, res) => {
+  console.log(req.body);
+  let result = new userModel(req.body);
+  await result.save();
+  res.redirect("/");
+});
+
+dbConnection();
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT || PORT}`);
+});
